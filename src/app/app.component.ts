397d95a4aa10b181ed4a768a4be387cc7655bf78
isogramc <theresa@isogramconsulting.com>
2020-05-24 17:52:27 +0200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component, VERSION } from '@angular/core';
 
+import {FormControl, FormGroup, FormBuilder, FormArray, Validators, AbstractControl, ValidatorFn} from '@angular/forms';
+
+function ageRangeValidator(min: number, max: number): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: boolean } | null => {
+    if (control.value !== undefined && (isNaN(control.value)) || control.value < min || control.value > max) {
+      return {ageRange: true};
+    }
+    return null;
+  };
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major + ' custom form input directive';
+  name = 'Angular ' + VERSION.major + ' custom form error validation and custom directive';
+  minAge = 18;
+  maxAge = 55;
+  loginForm: FormGroup;
+
+  constructor(){
+     this.loginForm = new FormGroup({
+       email: new FormControl(null, Validators.required),
+       password: new FormControl(null, [Validators.required, Validators.maxLength(8)]),
+       age: new FormControl(null, ageRangeValidator(this.minAge, this.maxAge))
+    });
+  }
+
+  ifLoginUser(){
+    console.log(this.loginForm.value);
+    console.log(this.loginForm.status);
+    // send to API etc.
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { PassportNumberDirective } from './passport-number.directive';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
   declarations: [ AppComponent, HelloComponent, PassportNumberDirective ],
   bootstrap:    [ AppComponent ]
 })
